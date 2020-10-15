3ddbdceb617db9c3c4aada9c6da5f3d9daeb2b0d
Mateusz Rosiak <215866@edu.p.lodz.pl>
2020-10-15 19:08:13 +0200

    Server initial commit

---
+++ MeTrail Server/src/main/java/com/mr/metrailserver/MeTrailServerApplication.java
@@ -0,0 +1,13 @@
+package com.mr.metrailserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeTrailServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeTrailServerApplication.class, args);
+    }
+
+}
