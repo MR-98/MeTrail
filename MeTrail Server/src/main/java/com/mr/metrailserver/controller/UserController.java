615711f6065885d128433524e483409823a0da94
Mateusz Rosiak <215866@edu.p.lodz.pl>
2020-10-16 16:52:03 +0200

    Server - Jwt authentication implemented

---
+++ MeTrail Server/src/main/java/com/mr/metrailserver/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.mr.metrailserver.controller;
+
+import com.mr.metrailserver.model.ApplicationUser;
+import com.mr.metrailserver.repository.ApplicationUserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private ApplicationUserRepository applicationUserRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(ApplicationUserRepository applicationUserRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.applicationUserRepository = applicationUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody ApplicationUser user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        applicationUserRepository.save(user);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+}
---
+++ MeTrail Server/src/main/java/com/mr/metrailserver/repository/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package com.mr.metrailserver.repository;
+
+import com.mr.metrailserver.model.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
