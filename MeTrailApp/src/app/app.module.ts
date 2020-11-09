4c1972a703c7c6adf5f0a6730d558049a7c9ccee
Mateusz Rosiak <215866@edu.p.lodz.pl>
2020-11-09 21:12:16 +0100

    Mobile app - home view implemented

--- MeTrailApp/src/app/app.module.ts
+++ MeTrailApp/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +20,9 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
   providers: [
     StatusBar,
     SplashScreen,
+    AndroidPermissions,
     Geolocation,
+    LocationAccuracy,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- MeTrailApp/src/app/components/home/home.component.ts
+++ MeTrailApp/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +13,13 @@ export class HomeComponent implements OnInit {
   latitude: any = 0; //latitude
   longitude: any = 0; //longitude
   interval: any;
+  drivingFactor: number = 0;
+  employeeName: string = 'Jan Kowalski';
 
-  constructor(private geolocation: Geolocation) { }
+  constructor(
+    private androidPermissions: AndroidPermissions,
+    private geolocation: Geolocation,
+    private locationAccuracy: LocationAccuracy) { }
 
   options = {
     timeout: 10000, 
@@ -21,18 +28,69 @@ export class HomeComponent implements OnInit {
   };
 
   ngOnInit() {
+    this.checkGPSPermission();
+   }
+
+  getCurrentCoordinates() {
     this.interval = setInterval(() => {
-      this.getCurrentCoordinates();
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.latitude = resp.coords.latitude;
+        this.longitude = resp.coords.longitude;
+       }).catch((error) => {
+         console.log('Error getting location', error);
+       });
     },3000)
   }
 
-  getCurrentCoordinates() {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.latitude = resp.coords.latitude;
-      this.longitude = resp.coords.longitude;
-     }).catch((error) => {
-       console.log('Error getting location', error);
-     });
+  //Check if application having GPS access permission  
+  checkGPSPermission() {
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then(
+      result => {
+        if (result.hasPermission) {
+
+          //If having permission show 'Turn On GPS' dialogue
+          this.askToTurnOnGPS();
+        } else {
+
+          //If not having permission ask for permission
+          this.requestGPSPermission();
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  requestGPSPermission() {
+    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
+      if (canRequest) {
+        console.log("4");
+      } else {
+        //Show 'GPS Permission Request' dialogue
+        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
+          .then(
+            () => {
+              // call method to turn on GPS
+              this.askToTurnOnGPS();
+            },
+            error => {
+              //Show alert if user click on 'No Thanks'
+              console.log('requestPermission Error requesting location permissions ' + error)
+            }
+          );
+      }
+    });
+  }
+
+  askToTurnOnGPS() {
+    this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
+      () => {
+        // When GPS Turned ON call method to get Accurate location coordinates
+        this.getCurrentCoordinates();
+      },
+      error => alert('Error requesting location permissions ' + JSON.stringify(error))
+    );
   }
 
 }
